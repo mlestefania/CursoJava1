9cc595e570f744867aa46d1145a54af0006f8082
Maria Laura <mlestefania@hotmail.com>
2023-10-27 18:46:32 -0300

    firrst commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+let js = 'amazing';
+if (js == 'amazing') alert('Javascript is FUN');
+console.log(40 + 8 + 3);
+
+
+let PI = 3.14
+let jonasMatilda = 'JM';
+console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun); */
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+// console.log(typeof true);
+
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+const now = 2027;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+console.log(25 - 10 - 5);
+console.log(25 - 5 - 10);
+
+let x, y;
+
+x = y = 25 - 10 - 5;
+
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
